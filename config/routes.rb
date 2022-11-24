4cafbac45081cbe9e1dc2f3b60dbe5d86ad7870b
Sergio Oseko <osk.sergio@gmail.com>
2022-11-23 22:03:25 -0300

    feat: import csv (erros - incomplete)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :titles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'titles/import_csv', to: 'titles#import_csv'
 end
